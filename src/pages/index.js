f510ce9e019c774bb1dd30417850e7d2590923af
Jeremy Tomlinson <jeremy.tomlinson@gmail.com>
2022-03-17 18:29:33 -0600

    new gatsby photo gallery

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,182 +1,74 @@
 import * as React from "react"
+import photos_logo from "../images/photos_logo.png"
+import ateam from "../images/a-team.jpg"
+import actionbronson from "../images/actionbronson.jpg"
+import bangarang_pimpire from "../images/bangarang-pimpire.jpg"
+import bicycle_kick from "../images/bicycle_kick.jpg"
+import bigbrother from "../images/bigbrother.jpg"
+import billydillinger from "../images/billydillinger.jpg"
+import butterfly_fractal from "../images/butterfly_fractal.jpg"
+import caps_lock from "../images/caps-lock.jpg"
+import carterbeauford from "../images/carterbeauford.jpg"
+import clutch_neil from "../images/clutch-neil.jpg"
+import coach_nelson from "../images/coach_nelson.jpg"
+import dannycarey from "../images/dannycarey.jpg"
+import davelombardo from "../images/davelombardo.jpg"
+import dead_cross from "../images/dead_cross.jpg"
+import dillinger from "../images/dillinger.jpg"
+import duckseason_rabbitseason from "../images/duckseason_rabbitseason.jpg"
+import epic_sax_guy from "../images/epic_sax_guy.jpg"
 
 // styles
 const pageStyles = {
   color: "#232129",
-  padding: 96,
+  padding: 20,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  backgroundColor: "#950efb",
 }
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
+const flexbox = {
+  display: "flex",
+  flexFlow: "row wrap",
 }
-const headingAccentStyles = {
-  color: "#663399",
+const margintwo = {
+  margin: 2,
 }
-const paragraphStyles = {
-  marginBottom: 48,
+const header = {
+  margin: "0",
+	textAlign: "center",
+	padding: "10px 0px 10px 0px",
 }
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
+const headerImg = {
+  maxWidth: "100%",
+  height: "auto",
 }
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
 
 // markup
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+      <div style={header}>
+          <img style={headerImg} src={photos_logo} alt="PHOTOS" />
+          <div style={flexbox}>
+              <img src={actionbronson} width="523.4" height="393.5" style={margintwo} />
+              <img src={ateam} width="523.4" height="393.5" style={margintwo} />
+              <img src={bangarang_pimpire} width="523.4" height="393.5" style={margintwo} />
+              <img src={bicycle_kick} width="504.3" height="379.2" style={margintwo} />
+              <img src={bigbrother} width="379.2" height="379.2" style={margintwo} />
+              <img src={billydillinger} width="504.3" height="379.2" style={margintwo} />
+              <img src={butterfly_fractal} width="379.2" height="379.2" style={margintwo} />
+              <img src={caps_lock} width="400.7" height="400.7" style={margintwo} />
+              <img src={carterbeauford} width="300.5" height="400.7" style={margintwo} />
+              <img src={clutch_neil} width="532.9" height="400.7" style={margintwo} />
+              <img src={coach_nelson} width="549.5" height="413.2" style={margintwo} />
+              <img src={dannycarey} width="469.6" height="353.1" style={margintwo} />
+              <img src={davelombardo} width="469.6" height="353.1" style={margintwo} />
+              <img src={dead_cross} width="353.1" height="353.1" style={margintwo} />
+              <img src={duckseason_rabbitseason} width="196.8" height="393.5" style={margintwo} />
+              <img src={dillinger} width="608.3" height="304.2" style={margintwo} />
+              <img src={epic_sax_guy} width="404.6" height="304.2" style={margintwo} />
+          </div>
+      </div>
     </main>
   )
 }
